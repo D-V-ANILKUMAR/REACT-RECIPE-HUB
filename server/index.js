19db6ad9d30bc449aec5e218131095255c1af82d
Sumanth789856 <dvsumanth30@gmail.com>
2026-03-24 21:39:46 +0530

    ok

--- server/index.js
+++ server/index.js
@@ -635,6 +635,15 @@ function getMockYouTubeVideos(query) {
   return videos;
 }
 
+// Serve frontend in production/testing
+const distPath = path.join(__dirname, '../dist');
+if (fs.existsSync(distPath)) {
+  app.use(express.static(distPath));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(distPath, 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`🍳 RecipeHub Server running on http://localhost:${PORT}`);
 });
