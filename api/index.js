168f3df07157eebd32d0cfc84e8fbb8757ad40d5
Sumanth789856 <dvsumanth30@gmail.com>
2026-03-25 07:29:30 +0530

    Library stability and simplified entry point

--- api/index.js
+++ api/index.js
@@ -1,3 +1,16 @@
-import app from '../server/index.js';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+// Temporary completely isolated ping route!
+app.get("/api/ping", (req, res) => res.json({ status: "isolated-pong" }));
+
+// Now try to import the rest of the app dynamically or just use it as it's being developed
+import mainApp from '../server/index.js';
+
+// Route everything else to the main app if it didn't crash
+app.use("/", mainApp);
 
 export default app;
